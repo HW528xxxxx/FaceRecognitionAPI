7220b6f5cb35c5d73e3f839e7baa3f332fd1cf64
施涵文 <o975o4733o@gmail.com>
2025-10-05 21:53:48 +0800

    安裝 uvicorn / fastapi

--- startup.sh
+++ startup.sh
@@ -2,11 +2,14 @@
 # 啟動虛擬環境
 source /home/site/wwwroot/antenv/bin/activate
 
-# 更新 pip
-/home/site/wwwroot/antenv/bin/python -m pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
+# 更新 pip、setuptools、wheel
+/home/site/wwwroot/antenv/bin/python -m pip install --upgrade pip setuptools wheel >> /home/LogFiles/startup.log 2>&1
 
 # 安裝 wheels 到虛擬環境
 /home/site/wwwroot/antenv/bin/python -m pip install /home/site/wwwroot/wheels/*.whl >> /home/LogFiles/startup.log 2>&1
 
-# 啟動 uvicorn，stdout/stderr 都導出
+# 安裝 uvicorn / fastapi
+/home/site/wwwroot/antenv/bin/python -m pip install fastapi uvicorn >> /home/LogFiles/startup.log 2>&1
+
+# 啟動 uvicorn
 exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 >> /home/LogFiles/startup.log 2>&1
