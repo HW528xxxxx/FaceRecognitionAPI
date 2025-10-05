c6e1ab63e7f94135dd34839d40a0520526969320
施涵文 <o975o4733o@gmail.com>
2025-10-05 16:44:19 +0800

    /home/LogFiles/startup.log 2>&1：把 stdout 和 stderr 都導出到同一個 log /home/LogFiles/ 是 Azure App Service Linux 容器預設的可讀寫 log 目錄 容器 crash 或 pip 安裝錯誤都會被記錄下來

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@
 # 啟動虛擬環境
 source /home/site/wwwroot/antenv/bin/activate
 
-# 安裝 wheels（確保所有依賴都安裝到虛擬環境）
-pip install --upgrade pip
-pip install /home/site/wwwroot/extracted/wheels/*.whl
+# 安裝 wheel，並導出 log
+pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
+pip install /home/site/wwwroot/extracted/wheels/*.whl >> /home/LogFiles/startup.log 2>&1
 
-# 用虛擬環境裡的 python 執行 uvicorn
-exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+# 啟動 uvicorn，stdout/stderr 都導出
+exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 >> /home/LogFiles/startup.log 2>&1
