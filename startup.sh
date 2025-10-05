18961ca593836bcff9429822aec848fa44354cec
施涵文 <o975o4733o@gmail.com>
2025-10-05 16:22:08 +0800

    把已經上傳的 dlib、face_recognition、numpy、pillow、click 都安裝到 antenv 裡

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # 啟動虛擬環境
 source /home/site/wwwroot/antenv/bin/activate
+
+# 安裝 wheels（確保所有依賴都安裝到虛擬環境）
+pip install --upgrade pip
+pip install /home/site/wwwroot/extracted/wheels/*.whl
+
 # 用虛擬環境裡的 python 執行 uvicorn
 exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
