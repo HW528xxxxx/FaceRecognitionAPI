b2c7f9dbe3ff37e56aaa6f863beb7e6f9e05c7b8
施涵文 <o975o4733o@gmail.com>
2025-10-05 17:35:58 +0800

    App Service 啟動時，一定要用虛擬環境的 Python 安裝 wheel，完整路徑都要寫出來，不能靠 pip 直接呼叫

--- startup.sh
+++ startup.sh
@@ -2,9 +2,11 @@
 # 啟動虛擬環境
 source /home/site/wwwroot/antenv/bin/activate
 
-# 安裝 wheel，並導出 log
-pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
-pip install /home/site/wwwroot/extracted/wheels/*.whl >> /home/LogFiles/startup.log 2>&1
+# 更新 pip
+/home/site/wwwroot/antenv/bin/python -m pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
+
+# 安裝 wheels 到虛擬環境
+/home/site/wwwroot/antenv/bin/python -m pip install /home/site/wwwroot/wheels/*.whl >> /home/LogFiles/startup.log 2>&1
 
 # 啟動 uvicorn，stdout/stderr 都導出
 exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 >> /home/LogFiles/startup.log 2>&1
