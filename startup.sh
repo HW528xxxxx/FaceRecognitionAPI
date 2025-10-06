e6f01b8c0e398fb2780f7296e85b6b5ae8aead63
施涵文 <o975o4733o@gmail.com>
2025-10-06 10:28:01 +0800

    startup.sh 需負責安裝 wheels 和 requirements → uvicorn 才能被找到並啟動

--- startup.sh
+++ startup.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+
+LOG_FILE=/home/LogFiles/startup.log
+
+# 如果虛擬環境不存在就建立
+if [ ! -d /home/site/wwwroot/antenv ]; then
+    python3 -m venv /home/site/wwwroot/antenv
+fi
+
 # 啟動虛擬環境
 source /home/site/wwwroot/antenv/bin/activate
 
 # 更新 pip
-/home/site/wwwroot/antenv/bin/python -m pip install --upgrade pip >> /home/LogFiles/startup.log 2>&1
+/home/site/wwwroot/antenv/bin/python -m pip install --upgrade pip >> $LOG_FILE 2>&1
 
-# 安裝 wheels 到虛擬環境
-/home/site/wwwroot/antenv/bin/python -m pip install /home/site/wwwroot/wheels/*.whl >> /home/LogFiles/startup.log 2>&1
+# 安裝 wheels
+/home/site/wwwroot/antenv/bin/python -m pip install /home/site/wwwroot/wheels/*.whl >> $LOG_FILE 2>&1
 
-# 啟動 uvicorn，stdout/stderr 都導出
-exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 >> /home/LogFiles/startup.log 2>&1
+# 安裝其他套件
+/home/site/wwwroot/antenv/bin/python -m pip install -r /home/site/wwwroot/requirements.txt >> $LOG_FILE 2>&1
+
+# 啟動 uvicorn
+exec /home/site/wwwroot/antenv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 >> $LOG_FILE 2>&1
